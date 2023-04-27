5adac9c946cd0e905d7d4b46c01017dc2f97a961
Achieve-software <vtcbcdtyr@gmail.com>
2023-04-27 23:42:35 +0300

    esc

---
+++ index.js
@@ -0,0 +1,10 @@
+//*1     \' 	' 	Single quote        tek tırnak
+//*2     \" 	" 	Double quote        çift tırnak
+//*3      \\ 	\ 	Backslash            \ işareti koyar
+//*4      \b 	    Backspace           geriye doğru bir karakter siler bazen konsolda tarayıcıdan dolayı ? görünür
+//*5      \n     	New Line            yeni satıra geçer
+//*6       \t    	Horizontal Tabulator bir tab boşluk bırakır
+
+let mtn = 'Ali\'nin "evi" ve arabası\\ çok güzel.\b Ali\n okula\t gidiyor.';
+
+console.log(mtn);
